56c61dec706f4ed08e9060eb8bdfa3d39848132e
rakesh2884 <rakebist@gmail.com>
2024-03-23 13:14:24 +0530

    TODO

--- Django01/views.py
+++ Django01/views.py
@@ -1,19 +1,32 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 from rest_framework import permissions
 from .models import Test
 from .serializers import TestSerializer
 
 class TestListApiView(APIView):
     def post(self, request, *args, **kwargs):
-        '''
-        Create the Todo with given todo data
-        '''
-
         serializer = TestSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class TestDetailApiView(APIView):
+    def get(self,request,id):
+        test=get_object_or_404(Test,id=id)
+        serializer=TestSerializer(test)
+        return Response(serializer.data)
+    def delete(self, request, id):
+        test=get_object_or_404(Test,id=id)
+        test.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    def put(self, request,id):
+        test=get_object_or_404(Test, id=id)
+        serializer=TestSerializer(test, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
--- Django1/urls.py
+++ Django1/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from Django01.views import (
-    TestListApiView,)
+    TestListApiView,TestDetailApiView)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api', TestListApiView.as_view()),
+    path('api/<int:id>/', TestDetailApiView.as_view()),
 ]
